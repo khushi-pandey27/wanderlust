4d517227a2fcfb630d2084beaf56ea07171acc98
khushi-pandey27 <khushi.pandey271005@gmail.com>
2025-12-29 18:27:18 +0530

    Project

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userRouter = require("./routes/user.js");
 
 //const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl=process.env.ATLASDB_URL;
-
+//main
 main()
     .then(()=>{
         console.log("connected to DB");
